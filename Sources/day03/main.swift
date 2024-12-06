34f32796a01f2d90b4a1860dd61ced1c13f2689a
Tom <wasserfrosch1234@gmail.com>
2024-12-07 00:39:58 +0100

    solve day3

--- Sources/day03/main.swift
+++ Sources/day03/main.swift
@@ -1,13 +1,27 @@
 import Foundation
 
 func main() {
-    let fileURL = URL(fileURLWithPath: "input03.txt")
+    let fileURL = URL(fileURLWithPath: "inputs/input03.txt")
     guard let content = try? String(contentsOf: fileURL, encoding: .utf8) else {
         print("couldnt read file")
         return
     }
-    let lines = content.split(separator: "\n")
+    let mulRegex = /mul\(([0-9]+),([0-9]+)\)/
 
+    var offset: String.Index = content.startIndex
+    var sum = 0
+    while let match = try? mulRegex.firstMatch(in: content[offset...])  {
+        let num1 = Int(match.output.1)!
+        let num2 = Int(match.output.2)!
+
+        print("found \(match.output.0)")
+
+        sum += num1 * num2
+
+        offset = match.range.upperBound
+    }
+
+    print("sum = \(sum)")
 }
 
 main()
